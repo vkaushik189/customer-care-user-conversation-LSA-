bd9211ab6a579c4a472bcc76082fa2ab5da4dfb3
vkaushik189 <kaushik.vema@gmail.com>
2018-04-23 12:29:32 -0700

    adding LSA model

--- customer_conversation_lsa.py
+++ customer_conversation_lsa.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr 22 02:04:48 2018
-
 @author: kaushik
 """
 
@@ -27,6 +25,10 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from pylab import *
+
 
 #importing all files into a dataframe
 path = "C:\\git\\customer-conversation----LDA\\files"
@@ -72,6 +74,58 @@ def preprocess(doc):
     doc = ' '.join(lemma.lemmatize(w) for w in doc.split())
     return doc
 
+# to get in dataframe format
 newdf['conversation'] = newdf['conversation'].apply(lambda x:preprocess(x))
-
+#to get it in a list format
+newdf_list = [preprocess(doc) for doc in newdf['conversation']]
     
+
+
+#calculating tf-idf vectoriser
+vectorizer = TfidfVectorizer(max_df=0.8, max_features=4000,
+                             min_df=2, stop_words='english',
+                             use_idf=True)
+
+# Build the tfidf vectorizer from the training data ("fit"), and apply it 
+x_train_tfidf = vectorizer.fit_transform(newdf_list)
+print("  Actual number of tfidf features: %d" % x_train_tfidf.get_shape()[1])
+
+# Get the words that correspond to each of the features.
+feat_names = vectorizer.get_feature_names()
+
+
+
+
+
+lsa_model = TruncatedSVD(n_components=5)
+lsa_topic_matrix = lsa_model.fit_transform(x_train_tfidf)
+
+
+for compNum in range(0, 10):
+
+    comp = lsa_model.components_[compNum]
+    
+    # Sort the weights in the first component, and get the indeces
+    indeces = np.argsort(comp).tolist()
+    
+    # Reverse the indeces, so we have the largest weights first.
+    indeces.reverse()
+    
+    # Grab the top 10 terms which have the highest weight in this component.        
+    terms = [feat_names[weightIndex] for weightIndex in indeces[0:10]]    
+    weights = [comp[weightIndex] for weightIndex in indeces[0:10]]    
+   
+    # Display these terms and their weights as a horizontal bar graph.    
+    # The horizontal bar graph displays the first item on the bottom; reverse
+    # the order of the terms so the biggest one is on top.
+    terms.reverse()
+    weights.reverse()
+    positions = arange(10) + .5    # the bar centers on the y axis
+    
+    figure(compNum)
+    barh(positions, weights, align='center')
+    yticks(positions, terms)
+    xlabel('Weight')
+    title('Strongest terms for component %d' % (compNum))
+    grid(True)
+    show()
